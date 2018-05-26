64b9ca3f63628bcd00f40f75069d03d10976e33a
dmfbrew <gotkd21@gmail.com>
2018-05-25 20:30:43 -0500

    gather IP

--- config.py
+++ config.py
@@ -4,6 +4,9 @@ import os
 workers = int(os.environ.get('GUNICORN_PROCESSES', '3'))
 threads = int(os.environ.get('GUNICORN_THREADS', '1'))
 
+f = os.popen('ifconfig eth0 | grep "inet\ addr" | cut -d: -f2 | cut -d" " -f1')
+your_ip=f.read()
+print(your_ip)
 
 forwarded_allow_ips = '*'
 secure_scheme_headers = { 'X-Forwarded-Proto': 'https' }
